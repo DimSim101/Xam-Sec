c46656308344793dd0ed938fc8deb1c78962decc
David Aaron <dsaaron123@gmail.com>
2017-07-13 13:54:47 +1000

    Xam Sec project testing initial commit

---
+++ README.md
@@ -0,0 +1,24 @@
+#Xamarin Security - Vulnerable App:
+
+Repo which imports two vulnerable libraries:
+    1. System.Net.Security: 4.3.0
+    2. Square.OkHTTP3: 3.0.1
+
+Checking vulnerabilities using OWASP dependency-check and SafeNuGet.
+
+------------------------------------------------------------------------------
+
+Links / Sources:
+
+- OWASP Tools:
+    + https://www.owasp.org/index.php/OWASP_Dependency_Check
+    + http://docs.myget.org/docs/how-to/checking-nuget-package-vulnerabilities-with-owasp-safenuget
+
+- CVE's:
+
+    + https://github.com/aspnet/Announcements/issues/239
+
+    + http://www.cvedetails.com/cve/CVE-2016-2402/
+
+    + http://docs.myget.org/docs/how-to/checking-nuget-package-vulnerabilities-with-owasp-safenuget
+
---
+++ Xam-Sec/Xam_Sec.Android/MainActivity.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+using Android.App;
+using Android.Content.PM;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.OS;
+using System.Net.Security;
+using Square.OkHttp3;
+
+namespace Xam_Sec.Droid
+{
+	[Activity (Label = "Xam_Sec", Icon = "@drawable/icon", Theme="@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+	public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
+	{
+		protected override void OnCreate (Bundle bundle)
+		{
+			TabLayoutResource = Resource.Layout.Tabbar;
+			ToolbarResource = Resource.Layout.Toolbar; 
+
+			base.OnCreate (bundle);
+
+			global::Xamarin.Forms.Forms.Init (this, bundle);
+			LoadApplication (new Xam_Sec.App ());
+
+            System.IO.Stream stream = new System.IO.MemoryStream();
+            SslStream ns = new SslStream(stream);
+
+            Console.WriteLine("SYSTEM.NET.SECURITY: SslStream = " + ns.ToString());
+
+            CertificatePinner.Builder cpBuilder = new CertificatePinner.Builder();
+            CertificatePinner certPinner = cpBuilder.Build();
+
+            Console.WriteLine("OKHTTP3: CertificatePinner = " + certPinner.ToString());
+
+            Console.ReadLine();
+		}
+	}
+}
+
